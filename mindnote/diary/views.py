c3852834e07d20e96dcac27a2b23a9b9f6856670
이민진 <e.minjin00@gmail.com>
2021-08-03 21:12:20 +0900

    Feat: page_list, page_detail view 추가

--- mindnote/diary/views.py
+++ mindnote/diary/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Page
 
 # Create your views here.
+def page_list(request):
+    lists = Page.objects.all()
+    return render(request, 'diary/page_list.html', {'lists':lists})
+
+def page_detail(request, page_id):
+    data = Page.objects.get(id=page_id)
+    return render(request, 'diary/page_detail.html', {'data':data})
+
+    
