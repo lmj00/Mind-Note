e9b818c883906f0cc87a2770d93cef4bc0b05744
이민진 <e.minjin00@gmail.com>
2021-08-06 02:31:41 +0900

    Feat:  add .is_valid in views

--- mindnote/diary/views.py
+++ mindnote/diary/views.py
@@ -17,8 +17,9 @@ def info(request):
 def page_create(request):
     if request.method == 'POST':
         page_form = PageForm(request.POST)
-        new_page = page_form.save()
-        return redirect('page-detail', page_id=new_page.id) 
+        if page_form.is_valid():
+            new_page = page_form.save()
+            return redirect('page-detail', page_id=new_page.id) 
     else:
-        form = PageForm()
-        return render(request, 'diary/page_form.html', {'form':form})
+        page_form = PageForm()
+    return render(request, 'diary/page_form.html', {'form':page_form})
