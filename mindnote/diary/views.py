c496f6b5d7d09e9b7628bc3aa64037d4a92e7710
이민진 <e.minjin00@gmail.com>
2021-08-12 20:27:11 +0900

    Feat: modify to generic views (CreateView)

--- mindnote/diary/views.py
+++ mindnote/diary/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.core.paginator import Paginator
+from django.views.generic import CreateView
+from django.urls import reverse
 from .models import Page
 from .forms import PageForm
 
@@ -23,15 +25,13 @@ def info(request):
     return render(request, 'diary/info.html')    
 
 
-def page_create(request):
-    if request.method == 'POST':
-        page_form = PageForm(request.POST)
-        if page_form.is_valid():
-            new_page = page_form.save()
-            return redirect('page-detail', page_id=new_page.id) 
-    else:
-        page_form = PageForm()
-    return render(request, 'diary/page_form.html', {'form':page_form}) 
+class PageCreateView(CreateView):
+    model = Page
+    form_class = PageForm
+    template_name = 'diary/page_form.html'
+
+    def get_success_url(self):
+        return reverse('page-detail', kwargs={'page_id':self.object.id})
 
 
 def page_update(request, page_id):
