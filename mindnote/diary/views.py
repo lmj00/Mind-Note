4db2dce2c6e78e815d95c9dd668988e323708ce4
이민진 <e.minjin00@gmail.com>
2021-08-14 03:31:32 +0900

    Feat: simplify views

--- mindnote/diary/views.py
+++ mindnote/diary/views.py
@@ -8,16 +8,12 @@ from .forms import PageForm
 # Create your views here.
 class PageListView(ListView):
     model = Page
-    tempalte_name = 'diary/page_list.html'
     ordering = ['-dt_created']
     paginate_by = 8
-    page_kwarg = 'page'
     
 
 class PageDetailView(DetailView):
     model = Page
-    template_name = 'diary/page_detail.html'
-    pk_url_kwarg = 'page_id'
 
 
 def info(request):
@@ -27,30 +23,24 @@ def info(request):
 class PageCreateView(CreateView):
     model = Page
     form_class = PageForm
-    template_name = 'diary/page_form.html'
 
     def get_success_url(self):
-        return reverse('page-detail', kwargs={'page_id':self.object.id})
+        return reverse('page-detail', kwargs={'pk':self.object.id})
 
 
 class PageUpdateView(UpdateView):
     model = Page
-    form_class = PageForm()
-    template_name = 'diary/page_form.html'
-    pk_url_kwarg = 'page_id'
+    form_class = PageForm
 
     def get_success_url(self):
-        return reverse('page-detail', kwargs={'page_id':self.object.id})
+        return reverse('page-detail', kwargs={'pk':self.object.id})
 
 
 class PageDeleteView(DeleteView):
     model = Page
-    template_name = 'diary/page_confirm_delete.html'
-    pk_url_kwarg = 'page_id'
 
     def get_success_url(self):
         return reverse('page-list')
-
 
 
 def index(request):
